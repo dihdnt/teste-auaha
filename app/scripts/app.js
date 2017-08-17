2c920c6964d800c7c8cdc815fd76bb8a659923e5
Diego Dantas <alandiego.dantas@hotmail.com>
2017-08-17 15:24:39 -0300

    correção de bugs - estilizando carousel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,89 @@
+$(document).ready(function(){
+    $('.owl-vitrine').owlCarousel({
+        autoplay:true,
+        dots:false,
+        autoplayHoverPause:true,
+        autoplayTimeout:5000,
+        loop:true,
+        margin:10,
+        nav: true,
+        navText:[
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5,
+                loop:true,
+            }
+        },
+    });
+
+    $('.owl-main').owlCarousel({
+        autoplay:true,
+        dots:false,
+        autoplayHoverPause:true,
+        autoplayTimeout:5000,
+        loop:true,
+        margin:10,
+        nav: true,
+        navText:[
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+            },
+            600:{
+                items:1,
+                nav:true
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    $('.owl-marcas').owlCarousel({
+        autoplay:true,
+        dots:false,
+        autoplayHoverPause:true,
+        autoplayTimeout:5000,
+        loop:true,
+        margin:10,
+        nav: true,
+        navText:[
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+             0:{
+                items:3,
+                nav:true,
+            },
+            600:{
+                items:3,
+                nav:true,
+            },
+            1000:{
+                items:6,
+                nav:true,
+                loop:true
+            }
+        }
+    });
+
+});
 /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
  */
